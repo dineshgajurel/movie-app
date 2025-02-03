6adbb49b749f24e44a0ac68c97f9171943c25c81
Dinesh <dinesh.gajurel11@gmail.com>
2025-02-03 21:06:12 +0545

    fixes minor styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@ export const Pagination = ({page, totalPages, onPageChange}) => {
     console.log("Pagination page:", page); // Log the current page here
 
     return (
-      <div className="flex items-center justify-between bg-gray-900 text-white p-2 rounded-lg shadow-md w-full max-w-lg mx-auto">
+      <div className="flex items-center justify-between bg-gray-900 text-white p-2 m-5 rounded-lg shadow-md w-full max-w-lg mx-auto">
         <button onClick={() => onPageChange(page - 1)} disabled={page === 1} className={`w-10 h-10 flex items-center justify-center bg-gray-700 hover:bg-gray-600 rounded-md ${page === 1 ? 'cursor-not-allowed' : 'cursor-pointer'}`}>
           <img src="./prev.svg" alt="Previous" className="w-5 h-5" />
         </button>
